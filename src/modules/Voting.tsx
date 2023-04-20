be0bb59ce6e6048aaac71ea1f797eb10c14503f1
Guga Villar <grsv.21@gmail.com>
2023-04-20 20:30:33 -0300

    :sparkles: feat: voting page

---
+++ src/modules/Voting.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@chakra-ui/react'
+import Candidate from '@/common/components/Candidate'
+
+const Voting = () => {
+  return (
+    <Box
+      p={8}
+      maxW='80%'
+      marginRight='auto'
+      marginLeft='auto'
+    >
+      <Candidate />
+    </Box>
+  )
+}
+
+export default Voting
