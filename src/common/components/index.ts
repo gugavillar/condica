915751ec64c3a6b3bc3c966a39745b9930b262df
Guga Villar <grsv.21@gmail.com>
2023-04-21 20:35:45 -0300

    :recycle: export components index

---
+++ src/common/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Candidate } from './Candidate'
+export { default as Footer } from './Footer'
+export { default as IfComponent } from './IfComponent'
+export { default as VoterRegistration } from './VoterRegistration'
