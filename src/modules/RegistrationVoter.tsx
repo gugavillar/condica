f97a4793cac24144954e98f4be84a4f15ebfa983
Guga Villar <grsv.21@gmail.com>
2023-04-22 08:43:07 -0300

    :sparkles: validation component registration voter

--- src/modules/RegistrationVoter.tsx
+++ src/modules/RegistrationVoter.tsx
@@ -1,8 +1,37 @@
 import { Box, Heading } from '@chakra-ui/react'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 import { VoterRegistration } from '@/common/components'
+import { validateRegistrationAPI } from '@/services/registrations'
+import useErrorToast from '@/common/hooks/useErrorToast'
 
 const RegistrationVoter = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { push } = useRouter()
+  const voterHasAlreadyVoterToast = useErrorToast('Eleitor já votou. Procure o mesário para mais informações')
+
+  const handleValidateRegistrationAPI = async (registration: string) => {
+    try {
+      setIsLoading(true)
+
+      const response = await validateRegistrationAPI({ registration: registration })
+
+      if (!response.canVote) {
+        voterHasAlreadyVoterToast()
+        return setIsLoading(false)
+      }
+
+      setIsLoading(false)
+      return push('/votacao')
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+      return
+    }
+  }
+
   return (
     <Box
       mx='auto'
@@ -15,7 +44,10 @@ const RegistrationVoter = () => {
       >
         Insira o título de eleitor
       </Heading>
-      <VoterRegistration />
+      <VoterRegistration
+        isLoading={isLoading}
+        handleValidateRegistrationAPI={handleValidateRegistrationAPI}
+      />
     </Box>
   )
 }
